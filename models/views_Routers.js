86187d8c32f65e2b37f08f44326d5836fb508af4
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2022-12-03 22:15:32 -0600

    Update views_Routers.js
    Sarai Hannah Ajai had added the view_Alabama_SignUp_Registration.html and styles_Alabama_SignUp_Registration.css files to the Public HTML and CSS Folders. Also, she had added those route paths to the Controllers and Routes folders, as well dated on 12--03-22.

--- models/views_Routers.js
+++ models/views_Routers.js
@@ -3,10 +3,18 @@ const views_Controller = require('../controllers/views_Controller');
 const router = express.Router();
 
 router
+    .get('/', views_Controller.view_AboutUs);
+
+router
+    .get('/', views_Controller.view_Alabama_SignUp_Registration);
+
+router
+    .get('/', views_Controller.view_Alabama_SignUp_Voters);
+
+router
     .get('/', views_Controller.view_Home);
 
-    router
-    .get('/', views_Controller.view_Alabama_SignUp_Voters);
+
 
 
 module.exports = router;
