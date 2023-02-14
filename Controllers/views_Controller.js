345dbfd6a54bab5883b8454f5a573260bbef91f4
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-02-14 17:01:23 -0600

    Update views_Controller.js
    Sarai Hannah Ajai had added a new the "view_LoginRegistrationsConfirm_ByEmail" route controller to the Controllers/views_Controller.js files dated 02-14-23.

--- Controllers/views_Controller.js
+++ Controllers/views_Controller.js
@@ -5,15 +5,11 @@ const view_AboutUs = (req, res) => {
     res.sendFile(path.join(__dirname, '../views/view_AboutUs'));
 };
 
-const view_Login_Voters = (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/view_Login_Voters'));
-};
-
 const view_Alabama_SignUp_Registrations = (req, res) => {
     res.sendFile(path.join(__dirname, '../views/view_Alabama_SignUp_Registrations'));
 };
 
-const Alabama_SignUp_Voters = (req, res) => {
+const view_Alabama_SignUp_Voters = (req, res) => {
     res.sendFile(path.join(__dirname, './views/view_Alabama_SignUp_Voters'));
 };
 
@@ -21,13 +17,23 @@ const view_Home = (req, res) => {
     res.sendFile(path.join(__dirname, './views/view_Home.html'));
 };
 
+const view_Login_Voters = (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/view_Login_Voters'));
+};
+
+const view_LoginRegistrationsConfirm_ByEmail = (req, res) => {
+    res.sendFile(path.join(__dirname, './views/view_LoginRegistrationsConfirm_ByEmail'));
+};
+
+
+
 module.exports = {  
-    view_AboutUs, 
-    view_Login_Voters, 
+    view_AboutUs,
     view_Alabama_SignUp_Registrations,
     view_Alabama_SignUp_Voters,
-    view_Home
-    
+    view_Home,
+    view_Login_Voters, 
+    view_LoginRegistrationsConfirm_ByEmail   
     
 
 }
