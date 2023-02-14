53e59dbb7ee32b57dfbd73a4e3c4b1e5e2efb105
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-02-14 17:02:37 -0600

    Update views_Routers.js
    Sarai Hannah Ajai had added a new the "view_LoginRegistrationsConfirm_ByEmail" route to the models/views_Routers.js files dated 02-14-23.

--- models/views_Routers.js
+++ models/views_Routers.js
@@ -6,16 +6,23 @@ router
     .get('/', views_Controller.view_AboutUs);
 
 router
-    .get('/', views_Controller.view_Login_Voters);
-
-router
-    .get('/', views_Controller.view_Alabama_SignUp_Registration);
+    .get('/', views_Controller.view_Alabama_SignUp_Registrations);
 
 router
     .get('/', views_Controller.view_Alabama_SignUp_Voters);
 
 router
     .get('/', views_Controller.view_Home);
+
+router
+    .get('/', views_Controller.view_Login_Voters);
+
+router
+    .get('/', views_Controller.view_LoginRegistrationsConfirm_ByEmail);
+
+
+
+
 
 
 
