1415632a4ce40f0122f594c2a5db82ba8d65e6c6
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-02-19 21:14:39 -0600

    Update index.js
    Sarai Hannah Ajai had installed the following API libraries to the index.js file (back-end) server are as follows:
    1. connect-sqlite3,
    2. express-session,
    3. better-sqlite3,
    4.better-sqlite3-session-store,
    5. passport,
    6. passport-local.
    And, she add written the JavaScript codes language for Express-Session in order authenticate user login dated 02-19-23.

--- index.js
+++ index.js
@@ -17,11 +17,37 @@ which, she will be able to open a database connection and perform specified comm
 */
 const sqlite3 = require('sqlite3').verbose();
 
+//const SQLiteStore = require('connect-sqlite3')(express);
+const session = require('express-session');
+//const SQLiteStore = require('connect-sqlite3')(session);
+
+const sqliteDB = require("better-sqlite3");
+
+const Sqlite3SessionStore = require("better-sqlite3-session-store")(session);
+
+const passport = require('passport');
+
+var LocalStrategy = require('passport-local').Strategy;
+
+const db = new sqliteDB("user_Login_Sessions.db", { verbose: console.log('Session login has been successfully created.') });
+
+require('dotenv').config();
+const IONOS_SECRET_KEY = process.env.IONOS_SECRET_KEY;
+const EXPRESS_SESSION_KEY = process.env.EXPRESS_SESSION_KEY;
+const SESSION_MAX_AGE = process.env.SESSION_MAX_AGE;
+
 /*
 Sarai Hannah Ajai has defined the 'path' constant from using her written JavaScript programmatic codes which has assisted her to import the 
 'path' module library which has provided her a way of working with HTML directories and file paths.
 */
 const path = require('path');
+
+/*
+Sarai Hannah Ajai has imported the 'nodemailer' constant from using her written JavaScript programmatic codes which has allowed her
+to interact with nodemailer SMTP from the NODE.js application using the nodemailer module library. 
+*/
+const nodemailer = require('nodemailer');
+
 
 const alabamaSignUp_Controller = require('./models/alabamaSignUp_Router');
 
@@ -34,17 +60,17 @@ const port = 3001;
 
 /*
 Sarai Hannah Ajai has used the 'iVoteBallot' Express object in order to set her middleware for server static or
-public files which will invokes her 'accouNetricsApp.use()' method which is a method to confirgure her middlewares used
+public files which will invokes her 'iVoteBallotuse()' method which is a method to confirgure her middlewares used
 by those specified routes of the Express HTTP server object. Specifically, 'iVoteBallot.use()' acts, as a
 middleware in an Express module library.
 */
 iVoteBallotApp.use(express.json());
-iVoteBallotApp.use(express.urlencoded({ extended: true }));
+iVoteBallotApp.use(express.urlencoded({ extended: false }));
 
 /*
-Sarai Hannah Ajai has used the 'accouNetricsApp' Express object in order to set her middleware for server web pages routes
-files which will invokes her 'accouNetricsApp.use()' method which is a method to confirgure her middlewares used by those 
-specified routes of the Express HTTP server object. Specifically, 'accouNetricsApp.use()' acts, as a middleware in
+Sarai Hannah Ajai has used the 'VoteBallot's Express object in order to set her middleware for server web pages routes
+files which will invokes her 'iVoteBallotApp.use()' method which is a method to confirgure her middlewares used by those 
+specified routes of the Express HTTP server object. Specifically, 'iVoteBallotApp.use()' acts, as a middleware in
 an Express module library. */
 
 iVoteBallotApp.use('/alabamasignup', require('./models/alabamaSignUp_Router'));
@@ -69,12 +95,52 @@ iVoteBallotApp.use(express.static(__dirname + '/public/views'));
 
 
 /*
-Sarai Hannah Ajai has used the 'accouNetricsApp' Express object; in order to set her middleware which will invokes her 
-'accouNetricsApp.get() method whenever there is a HTTP GET request with the path ('path.join(__dirname, '/public/view_ContactUs.html')
+Sarai Hannah Ajai has used the 'iVoteBallot' Express object; in order to set her middleware which will invokes her 
+'iVoteBallot.get() method whenever there is a HTTP GET request with the path ('path.join(__dirname, '/public/view_ContactUs.html')
 relative to the 'view_ContactUs' webpage root.
 */
 iVoteBallotApp.get('/alabamasignup', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/view_Home.html'));
     //console.log(res);
 });
+
+/*
+Sarai Hannah Ajai will need the ability to indentify user[s] authentications login; as well as, while the user[s] are browsing from iVoteBallot'
+webpages through a series of a HTTP's requests and responses to which, are associated with the same user[s] SESSION COOKIES from a HTTP's requests. 
+So, the SESSION COOKIES will allow user[s] HTTP's requests and responses LOGIN state to be maintained between the iVoteBallot's application server 
+(index.js) and the user[s] device browser[s]. And, the SESSION COOKIES is established by setting an user[s] LOGIN HTTP's requests in his/her device
+browser then the user[s] HTTP's requests are tranmitted to the iVoteBallot's back-end server (index.js) on every HTTP's requests. And, the 
+iVoteBallot's back-end server (index.js) uses the value assigned SESSION COOKIES; in order to retrieve the user[s] SQLite3 database information it 
+needs across multiple requests. Therefore, the SESSION COOKIES creates a stateful protocol to of HTTP is as follows:
+
+iVoteBallot must initializes the SESSION support; in order to make use of user[s] HTTP's LOGIN requests SESSION COOKIES, and the EXPRESS API
+application, SESSION COOKIES support is added by using EXPRESS-SESSION middleware.
+
+*/
+iVoteBallotApp.use(
+	session({
+		store: new Sqlite3SessionStore ({
+			client: db, 
+			collection: 'user_Login_Session.db',	
+			dir: 'database_Alabama_SignUp.db',
+			
+			cookie: {
+				secure: true,
+				httpOnly: true,
+				maxAge: parseInt(SESSION_MAX_AGE) // 1 hour or millisecond
+			
+			}			
+		}),
+		secret: EXPRESS_SESSION_KEY,		
+		saveUninitialized: false, 
+		resave: false,
+
+	})		
+);
+
+/*
+Initalize Passport and restore authentication state, if any, from the session.
+*/
+iVoteBallotApp.use(passport.initialize());
+iVoteBallotApp.use(passport.session());
 
